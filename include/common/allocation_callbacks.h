02f7bee19c9831325b6d433dde3488c51b5ee368
ravi688 <rp0412204@gmail.com>
2024-05-29 04:31:24 +0530

    [New] Added INLINE_IF_RELEASE_MODE macro  - this expands to __attribute__((always_inline)) if neither of GLOBAL_DEBUG and DEBUG is defined.  - otherwise, it expands to just empty

---
+++ include/common/allocation_callbacks.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <common/defines.h>
+
+/* allocation callbacks made during the compilation process
+ * one can initialize them with his/her user-defined allocators */
+typedef struct com_allocation_callbacks_t
+{
+	/* pointer to the user data which might be needed by 'allocate', 'reallocate', or 'deallocate' functions */
+	void* user_data;
+	/* called when allocation of certain size is needed */
+	void* (*allocate)(void* user_data, u32 size, u32 align);
+	/* called when reallocation of certain size is needed */
+	void* (*reallocate)(void* user_data, void* old_ptr, u32 size, u32 align);	
+	/* called when deallocation is needed for a memory block pointed by 'ptr' */
+	void (*deallocate)(void* user_data, void* ptr);
+} com_allocation_callbacks_t;
+
+COMMON_API com_allocation_callbacks_t com_allocation_callbacks_get_std();
+
+COMMON_API void* com_call_allocate_aligned(com_allocation_callbacks_t* callbacks, u32 size, u32 align);
+COMMON_API void* com_call_reallocate_aligned(com_allocation_callbacks_t* callbacks, void* old_ptr, u32 size, u32 align);
+#define com_allocate_obj_init(callbacks, type) CAST_TO(type*, com_memzero_forward(com_call_allocate(callbacks, sizeof(type)), sizeof(type)))
+#include <string.h> /* for memset */
+CAN_BE_UNUSED_FUNCTION static INLINE_IF_RELEASE_MODE void* com_memzero_forward(void* ptr, u32 size) { memset(ptr, 0, size); return ptr; }
+#define com_allocate_obj(callbacks, type) com_call_allocate_obj(callbacks, type)
+#define com_call_allocate_obj(callbacks, type) CAST_TO(type*, com_call_allocate(callbacks, sizeof(type)))
+CAN_BE_UNUSED_FUNCTION static INLINE_IF_RELEASE_MODE void* com_call_allocate(com_allocation_callbacks_t* callbacks, u32 size){ return com_call_allocate_aligned(callbacks, size, U32_MAX); }
+CAN_BE_UNUSED_FUNCTION static INLINE_IF_RELEASE_MODE void* com_call_reallocate(com_allocation_callbacks_t* callbacks, void* old_ptr, u32 size) { return  com_call_reallocate_aligned(callbacks, old_ptr, size, U32_MAX); }
+#define com_deallocate(callbacks, ptr) com_call_deallocate(callbacks, ptr)
+COMMON_API void com_call_deallocate(com_allocation_callbacks_t* callbacks, void* ptr);
+
+#include <bufferlib/buffer.h>
+COMMON_API buffer_t buf_create_with_callbacks(com_allocation_callbacks_t* callbacks, u32 element_size, u32 capacity, u32 offset);
+COMMON_API BUFFER* BUFcreate_with_callbacks(com_allocation_callbacks_t* callbacks, void* buffer, u32 element_size, u32 capacity, u32 offset);
+#define buf_new_with_callbacks(callbacks, type) buf_create_with_callbacks(callbacks, sizeof(type), 0, 0)
