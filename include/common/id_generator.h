6c92022dd3c5a64dd885fa17ef821fb70f55a704
ravi688 <rp0412204@gmail.com>
2024-07-17 23:12:36 +0530

    Use id_generator_id_type_t typedef instead of u32 directly in id generator class

--- include/common/id_generator.h
+++ include/common/id_generator.h
@@ -6,6 +6,9 @@
 
 typedef buffer_t unreserved_id_list_t;
 
+#define id_generator_id_type_t u32
+#define ID_GENERATOR_ID_TYPE_MAX U32_MAX
+
 typedef struct id_generator_t
 {
 	/* it will be needed when creating the unreserved list */
@@ -14,14 +17,14 @@ typedef struct id_generator_t
 	 * it is lazely allocated, i.e. the buffer_t object will be created upon the very first call to id_generator_return() */
 	unreserved_id_list_t* unreserved;
 	/* this is used to generate new ids, by incrementing it, if unreserved id list is empty */
-	u32 counter;
+	id_generator_id_type_t counter;
 } id_generator_t;
 
 BEGIN_CPP_COMPATIBLE
 
 /* creatse id_generator_t object
  * begin: no ids will be less than this, i.e. it will be starting value for 'counter' */
-static CAN_BE_UNUSED_FUNCTION INLINE_IF_RELEASE_MODE id_generator_t id_generator_create(u32 begin, com_allocation_callbacks_t* callbacks)
+static CAN_BE_UNUSED_FUNCTION INLINE_IF_RELEASE_MODE id_generator_t id_generator_create(id_generator_id_type_t begin, com_allocation_callbacks_t* callbacks)
 {
 	id_generator_t generator = { };
 	generator.callbacks = callbacks;
@@ -37,10 +40,10 @@ static CAN_BE_UNUSED_FUNCTION INLINE_IF_RELEASE_MODE void id_generator_destroy(i
 }
 
 /* returns a unique id in the scope of this generator */
-COMMON_API u32 id_generator_get(id_generator_t* generator);
+COMMON_API id_generator_id_type_t id_generator_get(id_generator_t* generator);
 /* puts the id into unreserved list so that it can be reused later, i.e. id_generator_get() might return it by looking into the list first */
-COMMON_API void id_generator_return(id_generator_t* generator, u32 id);
+COMMON_API void id_generator_return(id_generator_t* generator, id_generator_id_type_t id);
 /* resets the counter and clears the unreserved list, thus, invalidates all the ids which were returned by id_generator_get() */
-COMMON_API void id_generator_reset(id_generator_t* generator, u32 begin);
+COMMON_API void id_generator_reset(id_generator_t* generator, id_generator_id_type_t begin);
 
 END_CPP_COMPATIBLE
