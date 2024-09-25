bf651066be7f74ddae04fdbaa004000a422a221b
ravi688 <rp0412204@gmail.com>
2024-09-25 21:30:10 +0530

    Promoted TransparentHash and TransparentEqualTo to com:: namespace

---
+++ include/common/Concepts.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <concepts> // for std::is_convertible and std::is_constructible
+
+namespace com
+{
+	template<typename _ViewType, typename Type>
+	concept ViewType = std::is_convertible<Type, _ViewType>::value && std::is_constructible<Type, const _ViewType&>::value;
+}
