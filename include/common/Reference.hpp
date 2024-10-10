e35ab41ab19da5b06c8bc158e6c4115822c20cce
ravi688 <rp0412204@gmail.com>
2024-10-10 19:08:55 +0530

    Conversion operator resolution fixes in com::Reference and com::PtrReference

--- include/common/Reference.hpp
+++ include/common/Reference.hpp
@@ -5,7 +5,7 @@
 namespace com
 {
 	template<typename T>
-	concept cHandleType = std::constructible_from<T> && std::assignable_from<T&, T> && std::equality_comparable<T>;
+	concept cHandleType = std::constructible_from<T> && std::assignable_from<T&, T> && std::equality_comparable<T> && !std::same_as<T, bool>;
 
 	template<cHandleType T, T NullValue, typename... U>
 	class Reference
@@ -19,6 +19,12 @@ namespace com
 		constexpr Reference() noexcept : m_handle(Null) { }
 		constexpr Reference(T handle) noexcept : m_handle(handle) { }
 		constexpr Reference(const Reference& reference) noexcept : m_handle(reference.m_handle) { }
+
+		constexpr Reference& operator=(const T& handle) noexcept
+		{
+			m_handle = handle;
+			return *this;
+		}
 
 		constexpr Reference& operator =(const Reference& reference) noexcept
 		{
@@ -39,9 +45,23 @@ namespace com
 		constexpr const T& getHandle() const noexcept { return m_handle; }
 
 
-		constexpr explicit operator T&() noexcept requires(!std::is_pointer<T>::value) { return m_handle; }
-		constexpr explicit operator const T&() const noexcept requires(!std::is_pointer<T>::value) { return m_handle; }
-		constexpr explicit operator T() const noexcept requires(!std::is_pointer<T>::value) { return m_handle; }
+		// Enable the following set of 3 (explicit) conversion operator overloads only if handle type is of pointer type
+		// That's because implicit conversion of pointer to bool is possible and automatic in C++, especially in 'if' evaluation context
+		// Which also leads to ambiguous resolution with 'operator bool()''
+		// Therefore, we are bette defining these functions as 'explicit'
+		// Otherwise, disable them.
+		constexpr explicit operator T&() noexcept requires(std::is_pointer<T>::value) { return m_handle; }
+		constexpr explicit operator const T&() const noexcept requires(std::is_pointer<T>::value) { return m_handle; }
+		constexpr explicit operator T() const noexcept requires(std::is_pointer<T>::value) { return m_handle; }
+
+		// Enable the following set of 3 (implicit) conversion operator overloads only if handle type is not a pointer type
+		// That's because bool is automatically and implicitly converted to integer type in C++
+		// Therefore, if the destination type is an integral, so the handle type, and if no conversion operator found which convert
+		// to the integer type, C++ chooses the 'operator bool()' overload. Which leads to unexpected behaviour.
+		// Therefore, we define implicit conversion operators to the destination type so that C++ would priorities them first in the resolution.
+		constexpr operator T&() noexcept requires(!std::is_pointer<T>::value) { return m_handle; }
+		constexpr operator const T&() const noexcept requires(!std::is_pointer<T>::value) { return m_handle; }
+		constexpr operator T() const noexcept requires(!std::is_pointer<T>::value) { return m_handle; }
 		
 		constexpr operator bool() const noexcept { return m_handle != Null; }
 
