5bd77979efcd8abdf6cba17dcc176d9741885a5b
ravi688 <rp0412204@gmail.com>
2024-10-21 04:42:56 +0530

    Added com::OrderedEvent to allow defining an order of invoking event handlers.

--- include/common/defines.hpp
+++ include/common/defines.hpp
@@ -17,6 +17,9 @@
 
 namespace com
 {
+	// Only used in Event.hpp and OrderedEvent.hpp
+	struct no_publish_ptr_t { };
+
 	template<typename T> T& null_reference() { return *reinterpret_cast<T*>(NULL); }
 
 	#pragma GCC diagnostic push
