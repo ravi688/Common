1a83587ec6be921307cd5548143c0c0524fc1618
ravi688 <rp0412204@gmail.com>
2026-02-21 18:47:36 +0530

    Timing fixes in ProducerConsumerBuffer manual test

--- source/manual_tests/ProducerConsumerBuffer.cpp
+++ source/manual_tests/ProducerConsumerBuffer.cpp
@@ -4,11 +4,8 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
-#include <lock_guard>
 
 #include <iostream>
-
-std::mutex randomTimeMutex;
 
 int getRandomNumber()
 {
@@ -21,17 +18,29 @@ int getRandomNumber()
 	return value;
 }
 
-int getRandomTime()
+int getRandomTime(int min, int max)
 {
-	static std::random_device r;
-	static std::default_random_engine e1(r());
-	static std::uniform_int_distribution<int> uniform_dist(100, 1000);
+	static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	static std::mt19937 e1(seed);
+	static std::uniform_int_distribution<int> uniform_dist(min, max);
 
-	std::lock_guard<std::mutex> lock(randomTimeMutex);
 	int value = uniform_dist(e1);
 
 	return value;
 }
+
+int getRandomTime2(int min, int max)
+{
+	static unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+	static std::mt19937 e1(seed);
+	static std::uniform_int_distribution<int> uniform_dist(min, max);
+
+	int value = uniform_dist(e1);
+
+	return value;
+}
+
+
 
 std::mutex printMutex;
 
@@ -39,7 +48,7 @@ void thread1Handler(com::ProducerConsumerBuffer<int>& buffer)
 {
 	for(int i = 0; i < 50; ++i)
 	{
-		std::this_thread::sleep_for(std::chrono::duration<float, std::milli>(getRandomTime()));
+		std::this_thread::sleep_for(std::chrono::duration<float, std::milli>(getRandomTime(10, 100)));
 		int value = getRandomNumber();
 		buffer.push(value);
 		std::lock_guard<std::mutex> lock(printMutex);
@@ -49,9 +58,9 @@ void thread1Handler(com::ProducerConsumerBuffer<int>& buffer)
 
 void thread2Handler(com::ProducerConsumerBuffer<int>& buffer)
 {
-	std::this_thread::sleep_for(std::chrono::duration<float, std::milli>(getRandomTime()));
 	for(int i = 0; i < 50; ++i)
 	{
+		std::this_thread::sleep_for(std::chrono::duration<float, std::milli>(getRandomTime2(1000, 2000)));
 		int value = buffer.pop();
 		std::lock_guard<std::mutex> lock(printMutex);
 		std::cout << "[thread 2]: poped " << value << "\n";
@@ -60,7 +69,7 @@ void thread2Handler(com::ProducerConsumerBuffer<int>& buffer)
 
 int main()
 {
-	com::ProducerConsumerBuffer<int> m_buffer;
+	com::ProducerConsumerBuffer<int> m_buffer(10);
 	std::thread thread1(thread1Handler, std::ref(m_buffer));
 	std::thread thread2(thread2Handler, std::ref(m_buffer));
 	thread1.join();
